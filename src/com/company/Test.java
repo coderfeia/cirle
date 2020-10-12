ef1e39f207e725a85447708755f5980574400427
coderfeia <1061523323@qq.com>
2020-10-12 19:22:24 +0800

    12456

---
+++ src/com/company/Test.java
@@ -0,0 +1,14 @@
+package com.company;
+
+public class Test {
+    public static void main(String[] args){
+        System.out.println("   "+"*");
+        System.out.println("  "+"***");
+        System.out.println(" "+"*****");
+        System.out.println(""+"*******");
+        System.out.println(" "+"*****");
+        System.out.println("  "+"***");
+        System.out.println("   "+"*");
+
+    }
+}
